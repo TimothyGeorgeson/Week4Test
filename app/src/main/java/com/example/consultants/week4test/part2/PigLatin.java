fcc4303c76f4ab82fcd095f4193506f334a1cc7f
Timothy Georgeson <timothy.georgeson@mobileappscompany.us>
2018-11-16 13:05:10 -0500

    Fixing issue with 2nd part of test

--- app/src/main/java/com/example/consultants/week4test/part2/PigLatin.java
+++ app/src/main/java/com/example/consultants/week4test/part2/PigLatin.java
@@ -4,12 +4,11 @@ public class PigLatin {
 
     public static void main(String[] args) {
 
-        convert("chicken soup");
+        System.out.println(convert("chicken soup"));
 
     }
 
-    private static String convert(String input)
-    {
+    private static String convert(String input) {
         String[] splitStr = input.split(" ");
         String result = "";
 
@@ -17,44 +16,30 @@ public class PigLatin {
             result += convertToPigLatin(splitStr[i]);
         }
 
-        System.out.println(result);
-
-        return "";
+        return result;
     }
 
-    private static String convertToPigLatin(String input)
-    {
+    private static String convertToPigLatin(String input) {
         StringBuilder firstPart = new StringBuilder();
         StringBuilder secondPart = new StringBuilder();
-        StringBuilder result = new StringBuilder();
         int index = -1;
 
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ' ')
-            {
-                if (!(firstPart.toString().equals("") && (secondPart.toString().equals(""))))
-                {
-                    return result.append(secondPart).append(firstPart).append("ay").toString();
-                }
-            } else{
-                if (isVowel(input.charAt(i)) && index == -1) {
-                    secondPart.append(input.charAt(i));
-                    index = i;
-                } else if(index == -1){
-                    firstPart.append(input.charAt(i));
-                } else {
-                    secondPart.append(input.charAt(i));
-                }
-            }
 
+            if (isVowel(input.charAt(i)) && index == -1) {
+                secondPart.append(input.charAt(i));
+                index = i;
+            } else if (index == -1) {
+                firstPart.append(input.charAt(i));
+            } else {
+                secondPart.append(input.charAt(i));
+            }
         }
-        return "";
+        return secondPart.append(firstPart).append("ay ").toString();
     }
 
-    private static boolean isVowel(char c)
-    {
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-        {
+    private static boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             return true;
         }
         return false;
